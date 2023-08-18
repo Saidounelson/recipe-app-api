5d73fe77efacaaf4ca8e08528247edb98f024c88
Saidou Nassufou <saidou.nassufou@wizall.com>
2023-08-18 10:53:13 +0000

    add teste

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+"""
+    calculator function 
+    """
+
+
+def add(x, y):
+    """ 
+    add x and y return resultat
+    """
+    return x + y
+
+
+def substract(x, y):
+    """
+    substract x and y  return resultat
+    """
+    return y-x
