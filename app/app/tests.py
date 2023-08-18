2b03d3c41d8b18a75ad3623dcb4c2d286a0c3c6b
Saidou Nassufou <saidou.nassufou@wizall.com>
2023-08-18 11:09:57 +0000

    commente

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,26 +1,26 @@
-"""
-    Simple test
-    """
+# """
+#     Simple test
+#     """
 
-from django.test import SimpleTestCase
-from app.calc import add, substract
+# from django.test import SimpleTestCase
+# from app.calc import add, substract
 
 
-class CalcTests(SimpleTestCase):
-    """
-    Test calc module
-    """
+# class CalcTests(SimpleTestCase):
+#     """
+#     Test calc module
+#     """
 
-    def test_add_number(self):
-        """
-        function test add number         
-        """
-        res = add(5, 6)
-        self.assertEqual(res, 11)
+#     def test_add_number(self):
+#         """
+#         function test add number
+#         """
+#         res = add(5, 6)
+#         self.assertEqual(res, 11)
 
-    def test_substract_number(self):
-        """
-        function substract number
-        """
-        res = substract(3, 9)
-        self.assertEqual(res, 6)
+#     def test_substract_number(self):
+#         """
+#         function substract number
+#         """
+#         res = substract(3, 9)
+#         self.assertEqual(res, 6)
