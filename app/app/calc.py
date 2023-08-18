23be4bff4b390b7f40af562157d7224c13a25e8b
Saidou Nassufou <saidou.nassufou@wizall.com>
2023-08-18 11:01:11 +0000

    fix space

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,11 +7,11 @@ def add(x, y):
     """ 
     add x and y return resultat
     """
-    return x + y
+    return x+y
 
 
 def substract(x, y):
     """
     substract x and y  return resultat
     """
-    return y - x
+    return y-x
